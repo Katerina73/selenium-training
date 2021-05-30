ecc215a286da8d5f6fcbf94d9220431ba1558990
Katerina73 <e.d.sheglova@gmail.com>
2021-05-30 19:53:25 +0300

    Update LoginPageTest.java

--- src/test/java/ru/litecart/LoginPageTest.java
+++ src/test/java/ru/litecart/LoginPageTest.java
@@ -20,7 +20,7 @@ public class LoginPageTest {
 
     @Test
     public void myFirstTest() {
-        driver.get("http://localhost/litecart/admin/login.php?redirect_url=%2Flitecart%2Fadmin%2F");
+        driver.get("http://localhost/litecart/admin");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
